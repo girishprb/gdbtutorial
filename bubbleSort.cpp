20cdec6798b36f0dae062de21e249389e7fe0488
girishprb <gprabhu@ncsu.edu>
2014-02-01 20:25:04 -0500

    bubble sort v1

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
 
+
 int main()
 {
-	cout<<"Hello world";
+	int a[10],i;
+	for(i=0; i<10; i++)
+	{
+		a[i] = 10 - i;
+	}
+        for(i=0; i<10; i++)
+        {
+                cout<<a[i]<<endl;
+        }
+
 	return 0;
 }
