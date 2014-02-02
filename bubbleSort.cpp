3aa4f76ecc141c770a46ce85f7ca0b3729921c7c
girishprb <gprabhu@ncsu.edu>
2014-02-01 20:31:42 -0500

    bubbleSort v3

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -4,15 +4,25 @@ using namespace std;
 
 int main()
 {
-	int a[10],i;
-	for(i=0; i<10; i++)
+	int a[10],i,j, temp;
+	for(i=0; i<9; i++)
 	{
 		a[i] = 10 - i;
 	}
         for(i=0; i<10; i++)
         {
-                cout<<a[i]<<endl;
+	    for(j=i+1;j<10;j++)
+	    {
+	      if(a[i] > a[j])
+	      {
+		  temp = a[i];
+		  a[i] = a[j];
+		  a[j] = temp;
+	      }
         }
+        
+        for(i = 0; i < 10; i++)
+          cout<<a[i]<<endl;
 
 	return 0;
 }
